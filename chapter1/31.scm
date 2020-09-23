1396d2a6b5269bcdf1c899950af1aec25ff98b81
keaising <keaising@gmail.com>
2020-09-23 16:25:46 +0000

    update 31

--- chapter1/31.scm
+++ chapter1/31.scm
@@ -3,3 +3,25 @@
 		1
 		(* (term a)
 			(product term (next a) next b))))
+
+
+ (define (product-iter term a next b)
+ 	(define (iter a result)
+ 		(if (> a b)
+ 			result
+ 			(iter (next a) (* (term a) result)))
+ 	)
+ 	(iter a 1))
+
+
+(define (factorial n)
+	(product-iter (lambda (x) x) 1 (lambda (x) (+ x 1)) n))
+
+
+(define (fn n) (
+	/
+	(if (even? n) (+ n 2.0) (+ n 1))
+	(if (even? n) (+ n 1.0) (+ n 2))
+))
+
+(define (pi-4 n) (* 4 (product-iter fn 1 (lambda (x) (+ x 1)) n)))
--- chapter1/31_test.scm
+++ chapter1/31_test.scm
@@ -4,10 +4,10 @@
 (define (origin n) n)
 (define (inc n) (+ n 1))
 
-
-
-;(product origin 1 inc 5)
-
 (product origin 1 inc 5)
 
 
+(factorial 6)
+
+
+(pi-4 10000000)
